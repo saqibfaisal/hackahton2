8f99eef3388d607df200ec538ea636c1de279fd2
saqibfaisal <saqibfaisalofficial@gmail.com>
2023-02-19 05:30:14 -0800

    done

--- controller/login.js
+++ controller/login.js
@@ -39,35 +39,6 @@ const loginController = (req, res) => {
                     })
                 }
             })
-            // .then((password) => {
-            //     if (!password) {
-            //         return res.status(401).json({
-            //             message: "incorrect password"
-            //         })
-            //         // res.send({ message: " user successfully login", data: user })
-            //     }
-            //     else if (password) {
-            //         const token = jwt.sign({
-            //             username: result.username,
-            //             // email: user[0].email,
-            //         },
-            //             "login process",
-            //             {
-            //                 expiresIn: "24h"
-            //             })
-            //         res.status(200).json({
-            //             username: result.username,
-            //             // user: user[0].username,
-            //             // email: user[0].email,
-            //             token: token
-            //         })
-            //     }
-            //     else {
-            //         res.send({ error: "Invaild User" });
-            //     }
-            // }).catch((err) => {
-            //     res.send(err)
-            // })
         } else {
             res.json({ error: "Invaild User" })
         }
